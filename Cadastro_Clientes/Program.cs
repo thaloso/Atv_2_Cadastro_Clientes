6400b988da99a7e94d8038547163405b22ccc758
Thales Silva de Oliveira <thalessilva9@hotmail.com>
2022-12-16 20:52:55 -0300

    Implementação de cadastro Pessoa Física

--- Cadastro_Clientes/Program.cs
+++ Cadastro_Clientes/Program.cs
@@ -9,6 +9,27 @@ namespace Cadastro_Clientes
     {
         static void Main(string[] args)
         {
+            float val_pag;
+
+            Console.WriteLine("-------------Cadastro de Clientes-------------");
+            Console.WriteLine("Digite o nome do cliente:");
+            string var_nome = Console.ReadLine();
+            Console.WriteLine("O cliente é Pessoa Física ou Jurídidca?\n Digite (f) para Física e (j) para Jurídica.");
+            string var_tipo = Console.ReadLine();
+
+            switch (var_tipo)
+            {
+                case "f":
+                Pessoa_Fisica pf = new Pessoa_Fisica();
+                pf.nome = var_nome;
+                Console.WriteLine("Digite o CPF: ");
+                string var_cpf = Console.ReadLine();
+                pf.cpf = var_cpf;
+                Console.WriteLine("Digite a data de Nascimento:");
+                string var_nascimento = Console.ReadLine();
+                pf.data_nascimento = var_nascimento;
+                break;
+            }
             
             
 
