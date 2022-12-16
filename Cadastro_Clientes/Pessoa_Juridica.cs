25ffd25e01759b87caf60420ea21a66230e5522f
Thales Silva de Oliveira <thalessilva9@hotmail.com>
2022-12-16 19:40:44 -0300

    Inclusão de comentários no código

--- Cadastro_Clientes/Pessoa_Juridica.cs
+++ Cadastro_Clientes/Pessoa_Juridica.cs
@@ -7,8 +7,10 @@ namespace Cadastro_Clientes
         public string ie {get; set;}
 
         //Método do pagamento te imposto para pessoa jurídica
+        
         public override void Pagar_Imposto (float v)
         {
+            //override define que o método pagar_imposto irá reescrever caso o cliente seja pessoa jurídica
             this.valor = v;
             this.valor_imposto = this.valor * 20/100;
             this.total = this.valor + this.valor_imposto;
