78fe648a71a387ebd12718e30116f3cc04290cf1
Thales Silva de Oliveira <thalessilva9@hotmail.com>
2022-12-16 19:33:08 -0300

    Correção de código

--- Cadastro_Clientes/Pessoa_Fisica.cs
+++ Cadastro_Clientes/Pessoa_Fisica.cs
@@ -3,8 +3,8 @@ namespace Cadastro_Clientes
     class Pessoa_Fisica : Clientes //Pessoa física herda as propriedades de Clientes
     {
         //Atributos da pessoa física
-        public string cpf;
-        public string rg;
+        public string cpf {get; set;}
+        public string rg {get; set;}
 
 
 
