981c7bef59f0d17619e4f9558d23af6f0262bb58
Thales Silva de Oliveira <thalessilva9@hotmail.com>
2022-12-16 20:17:57 -0300

    Inicio do programa

--- Cadastro_Clientes/Program.cs
+++ Cadastro_Clientes/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Atividade solicitada para avaliação de aprendizagem do curso de programação back-end oferecido pelo SENAI
+using System; // Utilização da biblioteca System do C#
+
+namespace Cadastro_Clientes
+{
+    //Estrutura do programa
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa_Fisica pf = new Pessoa_Fisica();
+            
+
+        }
+
+    }
+
+
+}
